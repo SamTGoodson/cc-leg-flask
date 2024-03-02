063a80172cb7c93b5ad3f254758ffe98f1afc1b9
SamTGoodson <samtgoodson@gmail.com>
2024-03-02 10:20:20 -0500

    add dendrogram

--- blueprints/vote/routes.py
+++ blueprints/vote/routes.py
@@ -1,5 +1,9 @@
+import pandas as pd
 from flask import Blueprint, render_template
 from app.utils.db import get_db_connection
+from app.utils.cluster import start_hierarchical,create_dendrogram
+from app.utils.leg_api import find_close_votes
+from plotly.offline import plot 
 from . import vote_bp
 
 
@@ -10,7 +14,13 @@ def vote():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM vote;')  
-    vote_data = cur.fetchall()
+    vote_raw = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
     cur.close()
     conn.close()
-    return render_template('vote.html', data=vote_data)
+    df_vote = pd.DataFrame(vote_raw, columns=columns)
+    close_votes = find_close_votes(df_vote)
+    Z = start_hierarchical(close_votes)
+    dendrogram = create_dendrogram(Z)
+    dendrogram_div = plot(dendrogram, output_type='div', include_plotlyjs=False)
+    return render_template('vote.html', dendrogram=dendrogram_div)
